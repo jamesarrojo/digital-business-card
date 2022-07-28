5875c389bd077c5a25ad68f5a390a08f8360bb56
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-07-28 16:20:31 +0800

    initial Info code

---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <section className="info">
+            <img src="../assets/2x2.png" />
+            <h1>Isemman James Arrojo</h1>
+            <h3>Frontend Developer</h3>
+            <span>jamesarrojo.com</span>
+            <section className="buttons">
+                <a href="#"><i class="fa-solid fa-envelope"></i> Email</a>
+                <a href="#"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+            </section>
+            
+        </section>
+    )
+}
