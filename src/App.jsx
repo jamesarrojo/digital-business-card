951ff4eb9144aa32bef72e52ad9b7b5b9fcf57c9
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-07-28 19:11:25 +0800

    added the rest of the components but image can't be seen

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Info from "./components/Info"
+import About from './components/About'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
 
 function App() {
  
 
   return (
-   <Info />
+   <>
+    <Info />
+    <About />
+    <Interests />
+    <Footer />
+   </>
+   
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <section className="footer">
+            <a href="#"><img src="../assets/twitter.svg" alt="twitter icon" /></a>
+            <a href="#"><img src="../assets/facebook.svg" alt="facebook icon" /></a>
+            <a href="#"><img src="../assets/instagram.svg" alt="instagram icon" /></a>
+            <a href="#"><img src="@/assets/github.svg" alt="github icon" /></a>
+            
+        </section>
+    )
+}
